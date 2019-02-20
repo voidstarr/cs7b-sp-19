4409200f51e6ecc6be8310e88501c8defb39faaa
voidstarr <voidstarr@users.noreply.github.com>
2019-02-19 21:00:08 -0800

    spruced up Concentrations Grid class

--- Project01/concentrations/Concentrations.cpp
+++ Project01/concentrations/Concentrations.cpp
@@ -20,12 +20,43 @@ public:
       backingVector[i].resize(size);
     }
   }
-  bool inBounds(int r, int c) { return r < size && c < size && r >= 0 && c >= 0; }
+
+  Grid(initializer_list<initializer_list<int>> il) {
+    size = il.size();
+    backingVector.resize(size);
+    int i = 0;
+    for (auto a : il) {
+      if (a.size() != size)
+        throw "Bad dimensions of initializer_list for Grid! Not square!";
+      backingVector[i++] = vector<int>(a);
+    }
+  }
+
+  bool inBounds(int r, int c) {
+    return r < size && c < size && r >= 0 && c >= 0;
+  }
+
   vector<int> &operator[](int i) { return backingVector[i]; }
 
+  bool operator==(Grid &g) {
+    if (size != g.getSize())
+      return false;
+
+    for (int i = 0; i < backingVector.size(); i++)
+      for (int j = 0; j < backingVector[i].size(); j++)
+        if (backingVector[i][j] != g.getBackingVector()[i][j])
+          return false;
+
+    return true;
+  }
+
+  int getSize() { return size; }
+
+  vector<vector<int>> &getBackingVector() { return backingVector; }
+
   void print() {
-    for(int i = 0; i < size; i++) {
-      for(int j = 0; j < size; j++) {
+    for (int i = 0; i < backingVector.size(); i++) {
+      for (int j = 0; j < backingVector[i].size(); j++) {
         cout << backingVector[i][j] << " ";
       }
       cout << endl;
@@ -35,31 +66,27 @@ public:
 };
 
 void dropUnit(Grid &world, int row, int col) {
-  if (!world.inBounds(row,col)) return;
+  if (!world.inBounds(row, col))
+    return;
 
   if (++world[row][col] == 4) {
     world[row][col] = 0;
-    dropUnit(world, row+1,col);
-    dropUnit(world, row-1,col);
-    dropUnit(world, row,col+1);
-    dropUnit(world, row,col-1);
-    
+    dropUnit(world, row + 1, col);
+    dropUnit(world, row - 1, col);
+    dropUnit(world, row, col + 1);
+    dropUnit(world, row, col - 1);
   }
 }
 
 int main() {
-  Grid g(5);
+  Grid before({{0, 0, 0}, {1, 3, 1}, {0, 2, 0}});
+  Grid after({{0, 1, 0}, {2, 0, 2}, {0, 3, 0}});
 
-  g[3][2] = 3;
-  g[1][2] = 3;
-  g[2][2] = 3;
-  g[2][3] = 3;
-  g[2][1] = 3;
+  dropUnit(before, 1, 1);
 
-  g.print();
-  dropUnit(g, 2, 2);
-
-  g.print();
+  if (after == before) {
+    cout << "after == before" << endl;
+  }
 }
 
 /* * * * * * Tests Below This Point * * * * *
